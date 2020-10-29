246d29e0ea5e9a9c17195366cd727792784161b8
Farzane Dehghani <mis.dehghan@imtmc.local>
2020-10-29 08:35:01 +0330

    fix

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -77,7 +77,7 @@ class ApiController extends Controller
      * @return \Illuminate\Http\JsonResponse
      */
     public function createFile(Request $request){
-        $process = new Process(["touch /opt/myprogram/rererere.txt"]);
+        $process = new Process(["cat", " > /opt/myprogram/rererere.txt"]);
         $process->run();
 
         if (!$process->isSuccessful()) {
